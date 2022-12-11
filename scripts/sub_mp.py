3429b1dbc6af0a08722a76265a980806eed1102d
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-12-11 20:03:12 +0900

    passed check sub_mp

--- scripts/sub_mp.py
+++ scripts/sub_mp.py
@@ -1,2 +1,18 @@
 #!/usr/bin/env python3
 
+import rospy
+from std_msgs.msg import String
+
+def callback(msg):
+    rospy.loginfo("sub_test_Message '{}' recieved".format(msg.data))
+
+def subscriber():
+
+    rospy.init_node("sub_mp")   # ノードを初期化
+
+    rospy.Subscriber("medi", String, callback)     # 受信者を作成
+    
+    rospy.spin()    # ノードが終了するまで待機
+
+if __name__ == "__main__":
+    subscriber()
