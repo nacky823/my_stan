5d0f4e3731001c439c2bc23c3dc612e58d907cf6
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-12-12 12:55:23 +0900

    passed fix

--- scripts/subscribe_mediapipe.py
+++ scripts/subscribe_mediapipe.py
@@ -5,27 +5,30 @@ from geometry_msgs.msg import Point
 
 def main():
 
-    X_GAIN = 0.1
-
     rospy.init_node("subscribe")
     rospy.Subscriber("mediapipe_difference", Point, callback)
 
-    while not rospy.is_shutdown():
-
-        #fix_x = sub_diff.x * X_GAIN
-        #rospy.loginfo("recieved %f", float(fix_x))
-
-
-
-        rospy.sleep(0.1)
+    #rospy.sleep(0.1)
+    rospy.spin()
 
 def callback(msg):
+
+    X_GAIN = 0.1
+    Y_GAIN = 0.1
+    Z_GAIN = 0.1
+
     rospy.loginfo(msg)
     sub_diff = Point()
     sub_diff = msg
-    rospy.loginfo("recieved")
     rospy.loginfo(sub_diff)
+    rospy.loginfo("===================================================================")
 
+    fix_x = sub_diff.x * X_GAIN
+    rospy.loginfo("recieved %f", float(fix_x))
+    fix_y = sub_diff.y * Y_GAIN
+    rospy.loginfo("recieved %f", float(fix_y))
+    fix_z = sub_diff.z * Z_GAIN
+    rospy.loginfo("recieved %f", float(fix_z))
 
 
 
