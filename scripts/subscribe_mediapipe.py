930885e168168532167256b9a3254811a7db4a0b
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-12-12 13:27:21 +0900

    edit sub

--- scripts/subscribe_mediapipe.py
+++ scripts/subscribe_mediapipe.py
@@ -1,11 +1,34 @@
 #!/usr/bin/env python3
 
 import rospy
+import moveit_commander
 from geometry_msgs.msg import Point
 
 def main():
 
     rospy.init_node("subscribe")
+    crane = moveit_commander.RobotCommander()    # RobotCommander をインスタンス化
+    print("")
+    print("Robot Group names:  ", end="")        # crane_x7 の Group 全ての名前
+    print(crane.get_group_names())
+    print("Robot current state:")                # crane_x7 の現在の状態
+    print(crane.get_current_state())
+    arm = moveit_commander.MoveGroupCommander("arm")
+    arm.set_max_velocity_scaling_factor(0.1)
+    arm.set_max_acceleration_scaling_factor(1.0)
+    gripper = moveit_commander.MoveGroupCommander("gripper")
+    arm_init_pose = arm.get_current_pose().pose  # arm の初期姿勢
+    print("")
+    print("Arm initial pose:")
+    print(arm_init_pose)
+    print("home")
+    arm.set_named_target("home")
+    arm.go()
+    arm_current_pose = arm.get_current_pose().pose  # arm の現在姿勢$
+    print("")
+    print("Arm current pose:")
+    print(arm_current_pose)
+
     rospy.Subscriber("mediapipe_difference", Point, callback)
 
     #rospy.sleep(0.1)
