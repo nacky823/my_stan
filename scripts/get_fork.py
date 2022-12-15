fd3301e22bb849ab41657335c4f1d420de4df8eb
YazawaKenichi <tonfarken83825@gmail.com>
2022-12-15 19:42:32 +0900

    グリッパー開閉を記述中

---
+++ scripts/get_fork.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+# coding: utf-8
+
+import rospy
+import math
+
+def deg2rad(deg):
+    return math.radians(deg)
+
+def main():
+    GRIPPER_CLOSE = [0, 0]
+    # 開くのは 度数法 93 度が限界
+    GRIPPER_OPEN = [deg2rad(93), deg2rad(93)]
+    # ノードを初期化する
+    rospy.init_node('get_fork')
+    # gripper グループを取得する
+    gripper = moveit_commander.MoveGroupCommander("gripper")
+    # 最大関節速度を下げるらしい 0 ~ 1 で指定
+    gripper.set_max_velocity_scaling_factor(0.4)
+    # 最大関節加速度を下げるらしい 0 ~ 1 で指定
+    gripper.set_max_acceleration_scaling_factor(1.0)
+    # 目標値を設定
+    set_joint_value_target(GRIPPER_OPEN)
+    # 目標値に向けて動かす
+    gripper.go()
+    # キーが入力されるまで待つ
+    input("Input Key for Gripper Close ... ")
+
+if __name__ == "__main__":
+    main()
+
