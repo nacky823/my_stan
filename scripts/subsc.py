fb552668bbb3b90529fe32876e0f4dde2b940e0f
NAGAKI Yuki <youjiyongmu4@gmail.com>
2022-12-16 16:39:50 +0900

    add subsc

--- scripts/subsc.py
+++ scripts/subsc.py
@@ -3,32 +3,32 @@
 import rospy
 import moveit_commander
 from geometry_msgs.msg import Point
-from tf.transformations import quaternion_from_euler
 
 def main():
 
-    rospy.init_node("subscribe")
-    crane = moveit_commander.RobotCommander()    # RobotCommander をインスタンス化
+    rospy.init_node("subscribe")                       # node to start
+    crane = moveit_commander.RobotCommander()          # crane instantiation
     print("")
-    print("Robot Group names:  ", end="")        # crane_x7 の Group 全ての名前
-    print(crane.get_group_names())
-    print("Robot current state:")                # crane_x7 の現在の状態
-    print(crane.get_current_state())
-    arm = moveit_commander.MoveGroupCommander("arm")
-    arm.set_max_velocity_scaling_factor(0.1)
-    arm.set_max_acceleration_scaling_factor(1.0)
-    gripper = moveit_commander.MoveGroupCommander("gripper")
-    arm_init_pose = arm.get_current_pose().pose  # arm の初期姿勢
+    print("Robot Group names:  ", end="")
+    print(crane.get_group_names())                     # crane view [ all Group name ]
+    print("Robot current state:")
+    print(crane.get_current_state())                   # crane view current_state
+    arm = moveit_commander.MoveGroupCommander("arm")   # arm instantiation
+    arm.set_max_velocity_scaling_factor(0.1)           # arm setting_max_velocity
+    arm.set_max_acceleration_scaling_factor(1.0)       # arm setting_max_acceleration
+    gripper = moveit_commander.MoveGroupCommander("gripper")    # gripper instantiation
+
+    arm_init_pose = arm.get_current_pose().pose        # arm view initial_pose
     print("")
     print("Arm initial pose:")
     print(arm_init_pose)
     print("home")
     arm.set_named_target("home")
-    arm.go()
+    arm.go()                                           # arm moving to "home"
 
-    get_rpy = arm.get_current_rpy() 
+    get_rpy = arm.get_current_rpy()                    # arm getting to [ roll, pitch, yaw ]
     print(get_rpy)
-    get_xyz = arm.get_current_pose().pose
+    get_xyz = arm.get_current_pose().pose              # arm getting to pose
     print(get_xyz)
     print("okokokokokok")
 
@@ -115,4 +115,5 @@ def callback(msg):
 
 
 
-
+if __name__ == "__main__":
+    main()
